5b16544ec41368ba9727aec8a4a1c3704258941e
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-13 22:36:48 +0530

    [chore] frontend app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import React from "react";
 import "./App.css";
+import reaperImage from "./assets/reaper.png";
+import ImageEditor from "./components/ImageEditor";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
+const App: React.FC = () => {
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-red-500/90 min-h-screen flex flex-col p-2">
+      <div className="flex items-center">
+        <img src={reaperImage} className="h-25 w-25" alt="Reaper Logo" />
+        <div className="flex flex-col">
+          <h1 className="text-4xl tracking-wide font-metal-mania text-black">
+            Reaper
+          </h1>
+          <h3 className="text-lg text-gray-200 font-metal-mania">
+            Slice & Dice
+          </h3>
+        </div>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+      <ImageEditor />
+    </div>
   );
-}
+};
 
 export default App;
