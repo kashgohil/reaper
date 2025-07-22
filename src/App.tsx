ca10f22af9ec9f5ff4e2faf3bb199d4e53e2280f
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-22 15:32:00 +0530

    [chore] change the layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import reaperImage from "./assets/reaper.png";
 import ImageEditor from "./components/ImageEditor";
 
 const App: React.FC = () => {
-  return (
-    <div className="bg-red-500/90 min-h-screen flex flex-col p-2">
-      <div className="flex items-center">
-        <img src={reaperImage} className="h-25 w-25" alt="Reaper Logo" />
-        <div className="flex flex-col">
-          <h1 className="text-4xl tracking-wide font-metal-mania text-black">
-            Reaper
-          </h1>
-          <h3 className="text-lg text-gray-200 font-metal-mania">
-            Slice & Dice
-          </h3>
-        </div>
-      </div>
-      <ImageEditor />
-    </div>
-  );
+	return (
+		<div className="bg-red-500/90 h-screen flex flex-col p-2">
+			<div className="flex items-center">
+				<img
+					src={reaperImage}
+					className="h-25 w-25"
+					alt="Reaper Logo"
+				/>
+				<div className="flex flex-col">
+					<h1 className="text-4xl tracking-wide font-metal-mania text-black">Reaper</h1>
+					<h3 className="text-lg text-gray-200 font-metal-mania">Slice & Dice</h3>
+				</div>
+			</div>
+			<ImageEditor />
+		</div>
+	);
 };
 
 export default App;
