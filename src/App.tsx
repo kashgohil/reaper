a73a3110136beac0724726fc4b3cb926b202bc03
Kashyap Gohil <kashyapgohil476@gmail.com>
2025-07-24 02:16:11 +0530

    [chore] logo aligned to center

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,17 @@ import ImageEditor from "./components/ImageEditor";
 const App: React.FC = () => {
 	return (
 		<div className="bg-red-500/90 h-screen flex flex-col p-2">
-			<div className="flex items-center">
-				<img
-					src={reaperImage}
-					className="h-25 w-25"
-					alt="Reaper Logo"
-				/>
-				<div className="flex flex-col">
-					<h1 className="text-4xl tracking-wide font-metal-mania text-black">Reaper</h1>
-					<h3 className="text-lg text-gray-200 font-metal-mania">Slice & Dice</h3>
+			<div className="flex items-center justify-center">
+				<div className="flex items-center gap-4">
+					<img
+						src={reaperImage}
+						className="h-25 w-25"
+						alt="Reaper Logo"
+					/>
+					<div className="flex flex-col">
+						<h1 className="text-4xl tracking-wide font-metal-mania text-black">Reaper</h1>
+						<h3 className="text-lg text-gray-200 font-metal-mania">Slice & Dice</h3>
+					</div>
 				</div>
 			</div>
 			<ImageEditor />
